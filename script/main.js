944f801e8ce5e203e009d78d162f40455867944f
Luis Farias <popshovit33@gmail.com>
2024-11-28 00:46:42 -0600

    erase fetch to test

--- script/main.js
+++ script/main.js
@@ -5,13 +5,13 @@
 fetch (`https://jsonplaceholder.typicode.com/users`)
    .then((resp) => resp.json())
    .then((data) => loadCards(data))
-   .catch(error => {
+ /*   .catch(error => {
     if (!resp.ok) {
       cardContainer.innerHTML += error("Error al cargar los usuarios. Inténtalo de nuevo más tarde. ");
       
     }
     
-  })
+  }) */
 
    function loadCards(data) {
     cardContainer.innerHTML = ""
